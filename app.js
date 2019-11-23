511e7f1a7a04492b3a25e0311898fc1f4521df28
Mark Dixon <35239092+ForeverThinking@users.noreply.github.com>
2019-11-23 23:59:47 +0000

    Start interacting with ISS API and set up homepage routing

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 // jshint esversion: 6
 
+// packages to include
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
 
+// initialise express app
 const app = express();
 
+// set CSS file location
+app.use(express.static("public"));
+
+// set home route
 app.get('/', (req, res) => {
-  res.send('Hello world!');
+  request('http://api.open-notify.org/astros.json', (error, response, body) => {
+    console.log(body);
+  });
 });
 
+// set server listening
 app.listen(3000, () => {
   console.log('Server running on port 3000');
 });
