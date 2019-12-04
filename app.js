f0b3811744eb6868b33115f487b870a26cab0f6e
Mark Dixon <35239092+ForeverThinking@users.noreply.github.com>
2019-12-04 14:00:59 +0000

    Add dynamic port for server

--- app.js
+++ app.js
@@ -50,6 +50,6 @@ app.get('/info', (req, res) => {
 });
 
 // set server listening
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Server running on port 3000');
 });
