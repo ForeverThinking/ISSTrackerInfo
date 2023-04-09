3afbba4d74ae29b96e0e2a471de1a88fb7fb0d0e
ForeverThinking <35239092+ForeverThinking@users.noreply.github.com>
2023-04-09 17:23:56 +0100

    remove request and change to axios

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // packages to include
 const express = require('express');
 const bodyParser = require('body-parser');
-const request = require('request');
+const axios = require('axios');
 const ejs = require('ejs');
 
 // initialise express app
@@ -14,42 +14,37 @@ app.set('view engine', 'ejs');
 
 // set CSS file location and bodyparser setup
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
 app.use(express.static('public'));
 
 // set home route
 app.get('/', (req, res) => {
-  res.render('home');
+    res.render('home');
 });
 
 // set map route
 app.get('/map', (req, res) => {
-  res.render('map');
+    res.render('map');
 });
 
 // set info route
 app.get('/info', (req, res) => {
 
-  // GET request for People in Space API
-  request('http://api.open-notify.org/astros.json', (error, response, body) => {
-    // parse data from result
-    let data = JSON.parse(body);
+    axios.get('http://api.open-notify.org/astros.json')
+        .then((response) => {
+            let numberOfPeople = response.data.number;
+            let listOfPeople = response.data.people;
 
-    // get number of people and list of names from data
-    let numberOfPeople = data.number;
-    let listOfPeople = data.people; // use item.name when using forEach
-
-    // render info page and send data to use
-    res.render('info', {
-      numberOfPeople: numberOfPeople,
-      listOfPeople: listOfPeople
-    });
-  });
+            res.render('info', {
+                numberOfPeople: numberOfPeople,
+                listOfPeople: listOfPeople
+            });
+        });
 
 });
 
 // set server listening
 app.listen(process.env.PORT || 3000, () => {
-  console.log('Server running on port 3000');
+    console.log('Server running on port 3000');
 });
